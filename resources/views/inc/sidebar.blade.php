6ddc2ede3f35a6bd50277ba102d39980ed2cd331
HunterTerence <36937301+HunterTerence@users.noreply.github.com>
2019-09-26 16:14:49 +1000

    Add files via upload

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,11 +1,11 @@
 
 @section('sidebar')
 <div style="margin-top: 70px; margin-right: 10px;">
-    <img src="public/matchkey.png" height="180" width="200">
+    <img src="matchkey.png" height="180" width="200">
 </div>
 <br>
 @if(Session::get('userData'))
-<h2>Hi {{Session::get('userData')}}!</h2>
+<h2>Hi {{Session::get('userData')}} !</h2>
 <h3>Welcome to our website</h3>
 <p><a class="btn btn-secondary" href="{{ route('logout') }}" role="button" id="logout" >Logout &raquo;</a></p>
 @endif
