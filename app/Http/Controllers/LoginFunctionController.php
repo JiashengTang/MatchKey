fdee230564887948a5d58a19ae47d1250674c377
HunterTerence <36937301+HunterTerence@users.noreply.github.com>
2019-10-10 16:18:23 +1100

    Update LoginFunctionController.php

--- app/Http/Controllers/LoginFunctionController.php
+++ app/Http/Controllers/LoginFunctionController.php
@@ -43,7 +43,7 @@ class LoginFunctionController extends Controller
         //array_forget($array, 'names.joe');
         if($username=="admin" && $password=="admin")
         {
-            $request->session()->push('userData', $username);
+            $request->session()->put('userData', $username);
             $request->session()->put('userid', $IDList[0]);
             return redirect('/admin')->with('success','Login success!');
         }
